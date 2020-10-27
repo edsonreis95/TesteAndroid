2661565548cccc1cfe5753f6cf62e02f5226ed7c
Edson Reis <edson.reis95@gmail.com>
2020-10-27 10:15:15 -0300

    test revert

--- app/src/main/java/com/example/edson/testeandroid/application/ApplicationConstants.java
+++ app/src/main/java/com/example/edson/testeandroid/application/ApplicationConstants.java
@@ -2,7 +2,7 @@ package com.example.edson.testeandroid.application;
 
 public class ApplicationConstants {
 
-    public static final String SANTANDER_BASE_URL = "https://floating-mountain-50292.herokuapp.com";
+    public static final  String SANTANDER_BASE_URL = "https://floating-mountain-50292.herokuapp.com";
 
 
     public static final String APPLICATION_JSON = "application/json";
